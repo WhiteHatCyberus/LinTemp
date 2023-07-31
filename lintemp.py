a31464dd5eda963dd0dce03d48c5c8df400f4fde
Sleety Matt George <70995581+WhiteHatCyberus@users.noreply.github.com>
2023-07-31 13:40:55 +0530

    lintemp valpha released!

--- lintemp.py
+++ lintemp.py
@@ -1,58 +1,85 @@
-import subprocess
-import time
-from PIL import Image, ImageDraw
-import re
+import tkinter as tk
+import tkinter.messagebox as messagebox
+import psutil
+ascii_art = """
+          L       IIIIIII       N       N       TTTTTTTTT      EEEEEEEE        M         M   PPPPPP 
+         L         III         NN      N           T           E              MM       MM  P      P
+        L          III         N N     N           T           E              M M     M M  P      P
+       L           III         N  N    N           T           EEEEEEEE       M  M   M  M  PPPPPP 
+      L            III         N   N   N           T           E              M   M M   M  P      
+     L             III         N    N  N           T           E              M    M    M  P      
+    L              III         N     N N           T           EEEEEEEEE      M         M  P      
+   L               III         N      NN           T           E              M         M  P      
+  L                III         N       N           T           E              M         M  P      
+ L                IIIIIII      N       N           T           EEEEEEEE       M         M  P      
+LLLLLLLLLL       IIIIIII      N       N           T           EEEEEEEE       M         M  P      
+LLLLLLLLLL       IIIIIII      N       N           T           EEEEEEEEE      M         M  P      vAlpha
 
-BAR_WIDTH = 200  # Width of the temperature bar
-BAR_HEIGHT = 20  # Height of the temperature bar
-BORDER_WIDTH = 2  # Width of the bar borders
-REFRESH_RATE = 1  # Refresh rate in seconds
+"""
 
-def get_cpu_temperature():
-    result = subprocess.run(['sensors'], capture_output=True, text=True)
-    output = result.stdout
+print(ascii_art)
+print("Developed by White Hat Cyberus. For You.")
+def update_temperature(label):
+    try:
+        temperature = psutil.sensors_temperatures()['coretemp'][0].current
+        label.config(text=f"CPU Temperature: {temperature}°C")
 
-    temperature_line = re.search(r'Package.*?\+(\d+\.\d+)', output)
-    if temperature_line:
-        temperature = float(temperature_line.group(1))
-        return temperature
+        flag = getattr(update_temperature, "flag", 0)  # just making sure that's all, i swear its a feature not a bug. It works, so it works.
 
-    return None
-
-# Create an empty image with a transparent background
-image = Image.new('RGBA', (BAR_WIDTH, BAR_HEIGHT), (0, 0, 0, 0))
-draw = ImageDraw.Draw(image)
-
-def update_temperature_bar():
-    temperature = get_cpu_temperature()
-    if temperature is not None:
-        bar_fill = int((temperature / 100.0) * BAR_WIDTH)
-
-        # Draw the border of the bar with partial transparency
-        border_color = (255, 255, 255, 100)  # White with transparency (adjust the alpha value as desired)
-        draw.rectangle((0, 0, BAR_WIDTH - 1, BAR_HEIGHT - 1), outline=border_color, width=BORDER_WIDTH)
-
-        # Draw the filled portion of the bar with partial transparency
         if temperature >= 80:
-            fill_color = (255, 0, 0, 150)  # Red with transparency
-        elif temperature < 40:
-            fill_color = (0, 0, 255, 150)  # Blue with transparency
+            label.configure(bg='red')
+        
+            if flag != 2:  # Display warning only if the flag 
+                show_warning_dialog("High Temperature Warning", "I'm burning. Help!")
+                update_temperature.flag = 2  # flag is 1 to indicate that the warning is displayed already and doesn't have to display again.
+
+        elif temperature >= 70 and temperature < 80:
+            label.configure(bg='orange')
+            if flag != 1:  
+                show_warning_dialog("Potential Danger", "Jeez, It's getting hot in here.")
+                update_temperature.flag = 1  
+        
+        elif temperature >= 40 and temperature < 70:
+            label.configure(bg='green')
+            update_temperature.flag = 0  # Reset the flag to 0 when temperature is in the normal range
+
         else:
-            fill_color = (0, 255, 0, 150)  # Green with transparency
-        draw.rectangle((BORDER_WIDTH, BORDER_WIDTH, bar_fill, BAR_HEIGHT - BORDER_WIDTH), fill=fill_color)
+            label.configure(bg='blue')
+            if flag!=-1:
+                show_warning_dialog("Low temperature", "Brrrr, it's getting cold?")
+                update_temperature.flag = -1  
 
-        # Draw the temperature text inside the bar
-        temperature_text = f"{temperature}°C"
-        temperature_text_color = (255, 255, 255)  # White
-        text_bbox = draw.textbbox((0, 0), temperature_text)
-        text_position = ((BAR_WIDTH - text_bbox[2]) // 2, (BAR_HEIGHT - text_bbox[3]) // 2)
-        draw.text(text_position, temperature_text, fill=temperature_text_color)
+    except (IndexError, KeyError):
+        label.config(text="Cant obtain temperature info rn. Try again later.")
+        update_temperature.flag = 0  # big brain error handling, jesbin will be proud.
 
-        # Save the image to the local filesystem
-        image_path = '/tmp/temperature_bar.png'
-        image.save(image_path)
-        print(f"{temperature}°C")
+    label.after(2000, update_temperature, label)
 
-while True:
-    update_temperature_bar()
-    time.sleep(REFRESH_RATE)
+def show_warning_dialog(title, message):
+    messagebox.showwarning(title, message)
+
+def main():
+    root = tk.Tk()
+    root.overrideredirect(True)
+    root.wait_visibility(root)
+    root.wm_attributes('-alpha', 0.7)
+
+    label = tk.Label(root, text="Please wait, lintemp is checking...", fg='white', font=('Helvetica', 18)) #the text is formality btw, its instantly updated
+    label.pack()
+
+    label.bind("<ButtonPress-1>", lambda event: root.start_move(event)) 
+    '''
+    didnt make a window bar, so i made it so that you can drag the window by clicking anywhere on the window, sleety big brain moments!!
+    '''
+    label.bind("<B1-Motion>", lambda event: root.on_motion(event))
+
+    root.start_move = lambda event: setattr(root, '_x', event.x) or setattr(root, '_y', event.y)
+    root.on_motion = lambda event: root.geometry(f"+{root.winfo_x() + event.x - root._x}+{root.winfo_y() + event.y - root._y}")
+
+    update_temperature.flag = 0  # i actually did this to make sure the warnings are not spammed
+
+    update_temperature(label)
+
+    root.mainloop()
+
+main()
