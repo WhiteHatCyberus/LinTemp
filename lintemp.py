bf5c9dff1be96bb4ba1706f80f0537cad7d5bd2d
Sleety Matt George <70995581+WhiteHatCyberus@users.noreply.github.com>
2023-08-01 12:26:24 +0530

    Lintemp optimised running memory used

--- lintemp.py
+++ lintemp.py
@@ -19,19 +19,20 @@ LLLLLLLLLL       IIIIIII      N       N           T           EEEEEEEEE      M  
 
 print(ascii_art)
 print("Developed by White Hat Cyberus. For You.")
+
 def update_temperature(label):
     try:
         temperature = psutil.sensors_temperatures()['coretemp'][0].current
-        label.config(text=f"CPU Temperature: {temperature}°C")
+        label.config(text=f"CPU Temp: {temperature}°C")
 
-        flag = getattr(update_temperature, "flag", 0)  # just making sure that's all, i swear its a feature not a bug. It works, so it works.
+        flag = getattr(update_temperature, "flag", 0)       # just making sure that's all, i swear its a feature not a bug. It works, so it works.
 
         if temperature >= 80:
             label.configure(bg='red')
         
-            if flag != 2:  # Display warning only if the flag 
+            if flag != 2:
                 show_warning_dialog("High Temperature Warning", "I'm burning. Help!")
-                update_temperature.flag = 2  # flag is 1 to indicate that the warning is displayed already and doesn't have to display again.
+                update_temperature.flag = 2          # flag is 1 to indicate that the warning is displayed already and doesn't have to display again.
 
         elif temperature >= 70 and temperature < 80:
             label.configure(bg='orange')
@@ -41,17 +42,17 @@ def update_temperature(label):
         
         elif temperature >= 40 and temperature < 70:
             label.configure(bg='green')
-            update_temperature.flag = 0  # Reset the flag to 0 when temperature is in the normal range
+            update_temperature.flag = 0          # Reset the flag to 0 when temperature is in the normal range
 
         else:
             label.configure(bg='blue')
-            if flag!=-1:
+            if flag != -1:
                 show_warning_dialog("Low temperature", "Brrrr, it's getting cold?")
                 update_temperature.flag = -1  
 
     except (IndexError, KeyError):
         label.config(text="Cant obtain temperature info rn. Try again later.")
-        update_temperature.flag = 0  # big brain error handling, jesbin will be proud.
+        update_temperature.flag = 0         # big brain error handling, jesbin will be proud.
 
     label.after(2000, update_temperature, label)
 
@@ -64,22 +65,29 @@ def main():
     root.wait_visibility(root)
     root.wm_attributes('-alpha', 0.7)
 
-    label = tk.Label(root, text="Please wait, lintemp is checking...", fg='white', font=('Helvetica', 18)) #the text is formality btw, its instantly updated
+    label = tk.Label(root, text="Please wait, lintemp is checking...", fg='white', font=('Calibre', 13))        #the text is formality btw, its instantly updated
     label.pack()
 
-    label.bind("<ButtonPress-1>", lambda event: root.start_move(event)) 
+    def start_move(event):
+        root._x = event.x
+        root._y = event.y
+
+    def on_motion(event):
+        root.geometry(f"+{root.winfo_x() + event.x - root._x}+{root.winfo_y() + event.y - root._y}")
+
+    label.bind("<ButtonPress-1>", start_move)
+    label.bind("<B1-Motion>", on_motion)
     '''
     didnt make a window bar, so i made it so that you can drag the window by clicking anywhere on the window, sleety big brain moments!!
     '''
-    label.bind("<B1-Motion>", lambda event: root.on_motion(event))
+    update_temperature.flag = 0
 
-    root.start_move = lambda event: setattr(root, '_x', event.x) or setattr(root, '_y', event.y)
-    root.on_motion = lambda event: root.geometry(f"+{root.winfo_x() + event.x - root._x}+{root.winfo_y() + event.y - root._y}")
+    def close_app():
+        root.after_cancel(update_temperature)  # Stop the periodic update
+        root.destroy()
 
-    update_temperature.flag = 0  # i actually did this to make sure the warnings are not spammed
-
+    root.protocol("WM_DELETE_WINDOW", close_app)
     update_temperature(label)
-
     root.mainloop()
 
 main()
